a5e686d880ffc4520e40ab10bec86c165f388356
Spark <sparkismad@gmail.com>
2024-11-08 02:17:38 +0100

    changes in main file; WIP

--- bot.py
+++ bot.py
@@ -1,27 +1,99 @@
 import os
+import sys
 import discord
-import os
+import json
 from dotenv import load_dotenv
+import random
+from discord.ext import commands, tasks
+from utils.logger import setup_logger
+from database import DatabaseManager
+import aiosqlite
+import platform
 
 load_dotenv()
 
+if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
+        config = json.load(file)
+
 intents = discord.Intents.default()
 intents.message_content = True
+logger = setup_logger()
 
-client = discord.Client(intents=intents)
+class BeatbyBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(config["prefix"]),
+            intents=intents,
+            help_command=None,
+        )
+        
+        self.logger = logger
+        self.config = config
+        self.database = None
+        
+    async def init_db(self) -> None:
+        async with aiosqlite.connect(
+            f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
+        ) as db:
+            with open(
+                f"{os.path.realpath(os.path.dirname(__file__))}/database/schema.sql"
+            ) as file:
+                await db.executescript(file.read())
+            await db.commit()
+            
+    async def load_cogs(self) -> None:
+        """
+        The code in this function is executed whenever the bot will start.
+        """
+        for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+                    self.logger.info(f"Loaded extension '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    self.logger.error(
+                        f"Failed to load extension {extension}\n{exception}"
+                    )
+    
+    @tasks.loop(minutes=1.0)
+    async def status_task(self) -> None:
+        """
+        Setup the game status task of the bot.
+        """
+        statuses = ["with you!", "with Krypton!", "with humans!"]
+        await self.change_presence(activity=discord.Game(random.choice(statuses)))
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    print(f'{message.channel}: {message.author}: {message.author.name}: {message.content}')
-    print(f'{client.user}')
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(os.getenv("TOKEN"))
+    @status_task.before_loop
+    async def before_status_task(self) -> None:
+        """
+        Before starting the status changing task, we make sure the bot is ready
+        """
+        await self.wait_until_ready()
+        
+    async def setup_hook(self) -> None:
+        """
+        This will just be executed when the bot starts the first time.
+        """
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(
+            f"Running on: {platform.system()} {platform.release()} ({os.name})"
+        )
+        self.logger.info("-------------------")
+        await self.init_db()
+        await self.load_cogs()
+        self.status_task.start()
+        self.database = DatabaseManager(
+            connection=await aiosqlite.connect(
+                f"{os.path.realpath(os.path.dirname(__file__))}/database/database.db"
+            )
+        )
+        
+bot = BeatbyBot()
+bot.run(os.getenv("TOKEN"))
